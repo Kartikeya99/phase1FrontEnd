248c7c83e79ec587678ad5dfb066c44bdcac3a07
Kartikeya99 <kgkartikeyagokhale@gmail.com>
2018-05-19 16:16:02 +0530

    signup page changes and script

--- js/signup.js
+++ js/signup.js
@@ -1,21 +1,12 @@
 /* var baseUrl = 'http://quzebackend-dev.us-east-1.elasticbeanstalk.com'; */
-var baseUrl = 'http:localhost:8080';
+var baseUrl = 'https://backend.quze.co';
 
 var type = '';
 
 function signup() {
-	var email = $("#inputEmail").val();
+	var email = $("#inputUserEmail").val();
 	var userId = $("#inputUserName").val();
 	var password = $("#inputPassword").val();
-	
-	if($("#issuerYesNo").prop('checked') === true)
-	{
-		type = 'issuer';
-	}
-	else
-	{
-		type = 'recipient';
-	}
 
 	if(email === "" || userId === "" || password === "")
 	{
@@ -35,25 +26,25 @@ function signup() {
 		headers:headers,
 		success: function(result){
 			console.log(result);
-			if(result.token === "User Exists")
-			{
-				alert("User Already Exists. Please try another User Name");
-			}
-			else
-			{
-				localStorage.setItem('token',result.token);
-				localStorage.setItem('type',type);
-				if(type==='issuer')
-				{
-					localStorage.setItem('issuerId',userId);
-					window.location.replace("index.html");
-				}
-				else
-				{
-					localStorage.setItem('recipientId',userId);
-					window.location.replace("recipient.html");
-				}
-			}
+			// if(result.token === "User Exists")
+			// {
+			// 	alert("User Already Exists. Please try another User Name");
+			// }
+			// else
+			// {
+			// 	localStorage.setItem('token',result.token);
+			// 	localStorage.setItem('type',type);
+			// 	if(type==='issuer')
+			// 	{
+			// 		localStorage.setItem('issuerId',userId);
+			// 		window.location.replace("index.html");
+			// 	}
+			// 	else
+			// 	{
+			// 		localStorage.setItem('recipientId',userId);
+			// 		window.location.replace("recipient.html");
+			// 	}
+			// }
 		}
 	});}
 
@@ -63,14 +54,15 @@ $(document).ready(function(){
 });
 
 
-function typeOfLogin(cliciked_id){
-	// if (id == 'loginButton1st')
-	// 	type = 'issuer';
-	// else if (id == 'loginButton2nd')
-	// 	type = 'recipient';
-	// else
-	// 	type = 'employer';
+function typeOfLogin(clicked_id){
+	if (clicked_id === 'studentLoginButton')
+		type = 'recipient';
+	else if (clicked_id === 'institutionLoginButton')
+		type = 'issuer';
+	else
+		type = 'employer';
 
-	alert(cliciked_id);
+	$("#firstPage").css("display", "none");
+	$("#secondPage").css("display", "block");
 }
 
